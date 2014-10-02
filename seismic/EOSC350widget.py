583d1c8e90d5adb172ad25d5df212fd1d8382ffe
SEOGI KANG <sgkang09@gmail.com>
2014-10-02 09:02:10 -0700

    1. Make it cleaner 2. Reduce the number of imports

--- seismic/EOSC350widget.py
+++ seismic/EOSC350widget.py
@@ -2,6 +2,24 @@ from SimPEG.Utils import sub2ind, mkvc
 import scipy.io
 import numpy as np
 import matplotlib.pyplot as plt
+def ViewWiggle(syndata):
+    dx = 20
+    fig, ax = plt.subplots(1, 1, figsize=(7, 8))
+    kwargs = {
+    'skipt':1,
+    'scale': 0.05,
+    'lwidth': 1.,
+    'dx': dx,
+    'sampr': 0.004,
+    'clip' : dx*10.,
+    }
+    extent = [0., 38*dx, 1.0, 0.]
+
+    ax.invert_yaxis()
+    wiggle(syndata, ax = ax, **kwargs)
+    ax.set_xlabel("Offset (m)")
+    ax.set_ylabel("Time (s)")
+    ax.set_title("CMP gather")
 
 def NoisyNMOWidget(tintercept, v1, v2, v3):
     # data = scipy.io.loadmat('data_syn.mat')
@@ -85,25 +103,14 @@ def HyperbolicFun(tintercept, x, velocity):
     time = np.sqrt(x**2/velocity**2+tintercept**2)
     return time
 
-def stackallthree(data, tintercept, v1, v2, v3):
+def NMOstackthree(data, v1, v2, v3):
     dx = 20.
     xorig = np.arange(38)*dx
     time = np.load('time1.npy')
     traces = np.zeros((3,time.size))
     vtemp = np.r_[v1, v2, v3]
     for itry in range(3):
-
-        v = np.ones_like(time)*vtemp[itry]
-        Time = (time.reshape([1,-1])).repeat(data.shape[0], axis=0)
-        singletrace = np.zeros(data.shape[1])
-        print vtemp[itry]
-        for i in range(time.size):
-            toffset = np.sqrt(xorig**2/v[i]**2+time[i]**2)
-            Time = (time.reshape([1,-1])).repeat(data.shape[0], axis=0)
-            Toffset = (toffset.reshape([-1,1])).repeat(data.shape[1], axis=1)
-            indmin = np.argmin(abs(Time-Toffset), axis=1)
-            singletrace[i] = (mkvc(data)[sub2ind(data.shape, np.c_[np.arange(data.shape[0]), indmin])]).sum()
-        traces[itry,:] = singletrace
+        traces[itry,:] = NMOstack(data, xorig, time, vtemp[itry])
 
     fig, ax = plt.subplots(1, 3, figsize=(10, 8))
     kwargs = {
@@ -114,7 +121,7 @@ def stackallthree(data, tintercept, v1, v2, v3):
     'clip' : 10,
     }
     for i in range(3):
-        extent = [singletrace.min(), singletrace.max(), time.max(), time.min()]
+        extent = [traces[i,:].min(), traces[i,:].max(), time.max(), time.min()]
         ax[i].invert_yaxis()
         ax[i].axis(extent)
         wiggle(traces[i,:].reshape([1,-1]), ax=ax[i], **kwargs)
@@ -122,10 +129,7 @@ def stackallthree(data, tintercept, v1, v2, v3):
         ax[i].set_ylabel("Time (s)")
         ax[i].set_title(("Velocity = %6.1f")%(vtemp[i]))
 
-def stackall(data, tintercept, v):
-    dx = 20.
-    xorig = np.arange(38)*dx
-    time = np.load('time1.npy')
+def NMOstack(data, xorig, time, v):
     if np.isscalar(v):
         v = np.ones_like(time)*v
     Time = (time.reshape([1,-1])).repeat(data.shape[0], axis=0)
@@ -136,6 +140,13 @@ def stackall(data, tintercept, v):
         Toffset = (toffset.reshape([-1,1])).repeat(data.shape[1], axis=1)
         indmin = np.argmin(abs(Time-Toffset), axis=1)
         singletrace[i] = (mkvc(data)[sub2ind(data.shape, np.c_[np.arange(data.shape[0]), indmin])]).sum()
+    return singletrace
+
+def NMOstackSingle(data, v):
+    dx = 20.
+    xorig = np.arange(38)*dx
+    time = np.load('time1.npy')
+    singletrace = NMOstack(data, xorig, time, v)
 
     fig, ax = plt.subplots(1, 1, figsize=(7, 8))
     kwargs = {
@@ -153,34 +164,6 @@ def stackall(data, tintercept, v):
     ax.set_xlabel("Amplitude")
     ax.set_ylabel("Time (s)")
 
-def stack(data, xorig, time, tintercept, v):
-
-    toffset = np.sqrt(xorig**2/v**2+tintercept**2)
-    Time = (time.reshape([1,-1])).repeat(data.shape[0], axis=0)
-    Toffset = (toffset.reshape([-1,1])).repeat(data.shape[1], axis=1)
-    nwindow = 60
-    newdata = np.zeros_like(data)
-    indmin = np.argmin(abs(Time-Toffset), axis=1)
-    ind1 = np.arange(nwindow)+indmin[0]-int(0.5*nwindow)
-    singletrace = np.zeros(data.shape[1])
-    for i in range(indmin.size):
-        ind_temp = np.arange(nwindow)+indmin[i]-int(0.5*nwindow)
-        newdata[i, ind_temp] = data[i, ind_temp]
-        singletrace[ind1] = data[i, ind_temp] + singletrace[ind1]
-    fig, ax = plt.subplots(1, 1, figsize=(7, 8))
-    kwargs = {
-    'skipt':1,
-    'scale': 1.,
-    'lwidth': 1.,
-    'sampr': 0.004,
-    'ax': ax
-    }
-    extent = [singletrace.min(), singletrace.max(), time.max(), time.min()]
-    ax.invert_yaxis()
-    ax.axis(extent)
-    wiggle(singletrace.reshape([1,-1]), **kwargs)
-    ax.set_xlabel("Offset (m)")
-    ax.set_ylabel("Time (s)")
 
 def clipsign (value, clip):
   clipthese = abs(value) > clip
