e4b237518bfcaac208473a96d03139266b785bf9
Lindsey Heagy <lheagy@eos.ubc.ca>
2015-01-31 17:53:10 -0800

    Sorted synthetic seismogram and NMO notebooks into their own folders

--- seismic/SeisWidgetImport.py
+++
@@ -1,9 +0,0 @@
-from EOSC350widget import CleanNMOWidget, NoisyNMOWidget, NMOstackSingle, NMOstackthree, wiggle, ViewWiggle
-from IPython.display import set_matplotlib_formats
-from IPython.html.widgets import *
-import matplotlib
-set_matplotlib_formats('png')
-matplotlib.rcParams['savefig.dpi'] = 70 # Change this to adjust figure size
-import numpy as np
-syndata = np.load('syndata1.npy')
-obsdata = np.load('obsdata1.npy')
--- seismic/syntheticSeismogramImport.py
+++
@@ -1,8 +0,0 @@
-from IPython.display import set_matplotlib_formats
-from IPython.html.widgets import *
-import matplotlib
-from syntheticSeismogram import *
-from EOSC350widget import wiggle, ViewWiggle
-set_matplotlib_formats('png')
-matplotlib.rcParams['savefig.dpi'] = 100 # Change this to adjust figure size
-import numpy as np
