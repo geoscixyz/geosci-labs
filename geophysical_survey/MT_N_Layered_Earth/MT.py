9a49b9b6a6f8cc69c11e9e58b5fb9e1305ecabc2
Thibaut Astic <thast@eos.ubc.ca>
2016-02-12 15:53:14 -0800

    Update notebook with SimPEG path

--- geophysical_survey/MT_N_Layered_Earth/MT.py
+++ geophysical_survey/MT_N_Layered_Earth/MT.py
@@ -2,18 +2,52 @@ from scipy.constants import epsilon_0, mu_0
 import matplotlib.pyplot as plt
 import numpy as np
 from ipywidgets import *
+#from SimPEG.EM.Utils import k, omega 
 
-# Lindsey doing things:
+"""
+MT1D: n layered earth problem
+*****************************
 
-# from SimPEG.EM.Utils import k, omega 
+Author: Thibaut Astic
+Contact: thast@eos.ubc.ca
+Date: January 2016
+
+This code compute the analytic response of a n-layered Earth to a plane wave (Magneto-Tellurics).
+
+We start by looking at Maxwell's equations in the electric
+field \\\(\\\mathbf{E}\\) and the magnetic flux
+\\\(\\\mathbf{H}\\) to write the wave equations  
+\\(\\ \nabla ^2  \mathbf{E_x} + k^2 \mathbf{E_x} = 0 \\) &
+\\(\\ \nabla ^2  \mathbf{H_y} + k^2 \mathbf{H_y} = 0 \\)
+
+Then solving the equations in each layer "j" between z_{j-1} and z_j in the form of 
+\\(\\ E_{x,j} (z) = U_j e^{i k (z-z_{j-1})} + D_j e^{-i k (z-z_{j-1})} \\)
+\\(\\ H_{y,j} (z) = \frac{1}{Z_j} (D_j e^{-i k (z-z_{j-1})} - U_j e^{i k (z-z_{j-1})}) \\)
+
+With U and D the Up and Down components of the E-field.
+
+The iteration from one layer to another is ensure by: 
+
+\\(\\  \left(\begin{matrix} E_{x,j} \\ H_{y,j} \end{matrix} \right) = 
+    P_j T_j P^{-1}_J \left(\begin{matrix} E_{x,j+1} \\ H_{y,j+1} \end{matrix} \right) \\)
+
+And the Boundary Condition is set for the E-field in the last layer, with no Up component (=0) 
+and only a down component (=1 then normalized by the highest amplitude to ensure numeric stability)
+
+The layer 0 is assumed to be the air layer.
+
+"""
+
+#Frequency conversion
+omega = lambda f: 2.*np.pi*f
 
 #Evaluate k wavenumber
-kwave = lambda mu,sig,eps,f: np.sqrt(mu*mu_0*eps*epsilon_0*(2.*np.pi*f)**2.-1j*mu*mu_0*sig*2.*np.pi*f)
+k = lambda mu,sig,eps,f: np.sqrt(mu*mu_0*eps*epsilon_0*(2.*np.pi*f)**2.-1.j*mu*mu_0*sig*omega(f))
 
 #Define a frquency range for a survey
 frange = lambda minfreq, maxfreq, step: np.logspace(minfreq,maxfreq,num = step, base = 10.)
 
-#Functions to create random n-layered earth with physical properties
+#Functions to create random physical Perties for a n-layered earth
 thick = lambda minthick, maxthick, nlayer: np.append(np.array([1.2*10.**5]), 
                                                      np.ndarray.round(minthick + (maxthick-minthick)* np.random.rand(nlayer-1,1)
                                                             ,decimals =1))
@@ -30,18 +64,11 @@ eps = lambda mineps, maxeps, nlayer: np.append(np.array([1.]),
                                                np.ndarray.round(mineps + (maxeps-mineps)* np.random.rand(nlayer,1)
                                                                 ,decimals=1))
 
-#Evaluate Effective Conductivity - reomve this? 
-sighat = lambda sig,eps,f: sig - 1j*eps*epsilon_0*2.*np.pi*f
-
-#Evaluate wavelength - remove this? 
-wavelength = lambda k: 2.*np.pi/np.real(k)
-
-
 #Evaluate Impedance Z of a layer
-ImpZ = lambda f, mu, k: 2.*np.pi*f*mu*mu_0/k
+ImpZ = lambda f, mu, k: omega(f)*mu*mu_0/k
 
 #Complex Cole-Cole Conductivity - EM utils
-PCC= lambda siginf,m,t,c,f: siginf*(1.-(m/(1.+(1j*2.*np.pi*f*t)**c)))
+PCC= lambda siginf,m,t,c,f: siginf*(1.-(m/(1.+(1j*omega(f)*t)**c)))
 
 
 #Converted thickness array into top of layer array
@@ -55,32 +82,26 @@ def top(thick):
     
     return topv
 
-#Propagation Matrix
-def PropagationMatrixUD(z1,z2,h,k): 
-    
-    tran = np.matrix([[np.exp(-1j*k*h),0.],[0.,np.exp(1j*k*h)]],dtype='complex_')
-    
-    prop2 = np.matrix([[1.,1,],[-1./z2,1./z2]],dtype='complex_')
-    
-    prop1_1 = np.matrix([[1.,-z1],[1.,z1]],dtype='complex_')/2.
-    
-    PropMatUD = tran*prop1_1*prop2
-    
-    return PropMatUD
+#Propagation Matrix and theirs inverses
 
-transi = lambda h,k: np.matrix([[np.exp(-1j*k*h),0.],[0.,np.exp(1j*k*h)]],dtype='complex_')
+#matrix T for transition of Up and Down components accross a layer
+T = lambda h,k: np.matrix([[np.exp(1j*k*h),0.],[0.,np.exp(-1j*k*h)]],dtype='complex_')
 
-transi_1 = lambda h,k: np.matrix([[np.exp(1j*k*h),0.],[0.,np.exp(-1j*k*h)]],dtype='complex_')
+Tinv = lambda h,k: np.matrix([[np.exp(-1j*k*h),0.],[0.,np.exp(1j*k*h)]],dtype='complex_')
 
-prop = lambda z: np.matrix([[1.,1,],[-1./z,1./z]],dtype='complex_')
+#transition of Up and Down components accross a layer
+UD_Z = lambda UD,z,zj,k : T((z-zj),k)*UD
 
-propinv = lambda z: np.matrix([[1.,-z],[1.,z]],dtype='complex_')/2.
 
-UD_Z = lambda UD,z,zj,k : transi_1((z-zj),k)*UD
+#matrix P relating Up and Down components with E and H fields
+P = lambda z: np.matrix([[1.,1,],[-1./z,1./z]],dtype='complex_')
+
+Pinv = lambda z: np.matrix([[1.,-z],[1.,z]],dtype='complex_')/2.
+
 
 #Time Variation of E and H
-E_ZT = lambda U,D,f,t : np.exp(1j*2*np.pi*f*t)*(U+D)
-H_ZT = lambda U,D,Z,f,t : (1./Z)*np.exp(1j*2*np.pi*f*t)*(D-U)
+E_ZT = lambda U,D,f,t : np.exp(1j*omega(f)*t)*(U+D)
+H_ZT = lambda U,D,Z,f,t : (1./Z)*np.exp(1j*omega(f)*t)*(D-U)
 
 #Plot the configuration of the problem
 def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
@@ -95,8 +116,10 @@ def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
     ax.yaxis.tick_right()
     ax.yaxis.set_label_position("right")
     
+    #define filling for the different layers
     hatches=['/'  , '+', 'x', '|' , '\\', '-'  , 'o' , 'O' , '.' , '*' ] 
     
+    #Write the physical properties of air
     ax.annotate(("Air, $\sigma$ =%1.0f mS/m")%(sig[0]*10**(3)),
             xy=(-widthg/2., -np.abs(z.max())/2.), xycoords='data',
             xytext=(-widthg/2., -np.abs(z.max())/2.), textcoords='data',
@@ -111,10 +134,8 @@ def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
             xy=(-widthg/2., -np.abs(z.max())/3.), xycoords='data',
             xytext=(0, -np.abs(z.max())/3.), textcoords='data',
             fontsize=14.)
-    
-    #ax.set_title(('Time = % ns')%(sig[1]*1e9), fontsize = 16)
-
-    
+     
+    #Write the physical properties of the differents layers up to the (n-1)-th and fill it with pattern
     for i in range(1,len(topn)-1,1):
         if topn[i] == topn[i+1]:
             pass
@@ -136,12 +157,8 @@ def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
         
             ax.plot(widthn,topn[i]*np.ones_like(widthn),color='black')    
             ax.fill_between(widthn,topn[i],topn[i+1],alpha=0.3,color="none",edgecolor='black', hatch=hatches[(i-1)%10])
-        
-        #ax.annotate(("Layer, $\sigma$ = %10.0f S/m")%(sig[0]),
-        #    xy=(widthg/2., -np.abs(z.max())/2.), xycoords='data',
-        #    xytext=(widthg/2., -np.abs(z.max())/2.), textcoords='data',
-        #    fontsize=16.)
-        
+    
+    #Write the physical properties of the n-th layer and fill it with pattern
     ax.plot(widthn,topn[-1]*np.ones_like(widthn),color='black')    
     ax.fill_between(widthn,topn[-1],z.max(),alpha=0.3,color="none",edgecolor='black', hatch=hatches[(len(topn)-2)%10])
     
@@ -160,6 +177,7 @@ def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
             xytext=(-widthg/2., (2.*topn[-1]+z.max())/3), textcoords='data',
             fontsize=14.)
     
+    #plot Trees!
     ax.annotate("",
             xy=(widthg/2., -1.*z.max()/5.), xycoords='data',
             xytext=(widthg/2., 0.), textcoords='data',
@@ -195,176 +213,7 @@ def PlotConfiguration(thick,sig,eps,mu,ax,widthg,z):
             xytext=(1.2*widthg/2., 0.), textcoords='data',
             arrowprops=dict(arrowstyle='->, head_width=1.6,head_length=1.6',color='green',linewidth=2.)
             )
-    
-    ax.annotate("",
-            xy=(1.5*widthg/2., -1.*z.max()/5.), xycoords='data',
-            xytext=(1.5*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.2,head_length=1.2',color='green',linewidth=2.)
-            )
 
-    ax.annotate("",
-            xy=(1.5*widthg/2., -3./4.*z.max()/5.), xycoords='data',
-            xytext=(1.5*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.4,head_length=1.4',color='green',linewidth=2.)
-            )
-
-    ax.annotate("",
-            xy=(1.5*widthg/2., -1./2.*z.max()/5.), xycoords='data',
-            xytext=(1.5*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.6,head_length=1.6',color='green',linewidth=2.)
-            )
-
-    
-    ax.invert_yaxis()
-
-    return ax
-
-#Plot the general N layer case
-def PlotLayer(thick,ax,widthg,z):
-    
-    topn = top(thick)
-    widthn = np.arange(-widthg,widthg+widthg/10.,widthg/10.)
-    
-    ax.set_ylim([z.min(),z.max()])
-    ax.set_xlim([widthn.min(),widthn.max()])
-    
-    hatches=['/'  , '+', 'x', '|' , '\\', '-'  , 'o' , 'O' , '.' , '*' ] 
-    
-    ax.annotate(("Air,  $\sigma$=0 mS/m,  $\epsilon_r$=1,  $\mu_r$=1"),
-            xy=(-widthg/1.5, -np.abs(z.max())/4.), xycoords='data',
-            xytext=(-widthg/1.5, -np.abs(z.max())/4.), textcoords='data',
-            fontsize=20.)
-    
-    #ax.set_title(('Time = % ns')%(sig[1]*1e9), fontsize = 16)
-
-    ax.get_yaxis().set_ticks([])
-    ax.get_xaxis().set_ticks([])
-    
-    ax.axis('off')
-    
-    for i in range(1,len(topn)-2,1):
-        
-        ax.annotate("",
-            xy=(-widthg, topn[i]), xycoords='data',
-            xytext=(-widthg,topn[i+1]), textcoords='data',
-            arrowprops=dict(arrowstyle='<->',color='black',linewidth=2.))
-        
-        ax.annotate(("$h_%1i$")%((i)),
-            xy=(-widthg,(topn[i]+topn[i+1])/2), xycoords='data',
-            xytext=(-widthg,(topn[i]+topn[i+1])/2), textcoords='data',
-            fontsize=24.)
-        
-        ax.annotate(("$z_%1i$")%((i-1)),
-            xy=(widthg,topn[i]), xycoords='data',
-            xytext=(widthg,topn[i]), textcoords='data',
-            fontsize=24.)
-        
-        ax.annotate(("$\sigma_%1i$")%(i),
-            xy=(0., (topn[i]+topn[i+1])/2), xycoords='data',
-            xytext=(0., (topn[i]+topn[i+1])/2), textcoords='data',
-            fontsize=24.)
-    
-        ax.annotate(("$\epsilon_%1i$")%(i),
-            xy=(-widthg/1.5, (topn[i]+topn[i+1])/2), xycoords='data',
-            xytext=(-widthg/1.5, (topn[i]+topn[i+1])/2), textcoords='data',
-            fontsize=24.)
-    
-        ax.annotate(("$\mu_%1i$")%(i),
-            xy=(-widthg/2., (topn[i]+topn[i+1])/2), xycoords='data',
-            xytext=(-widthg/2., (topn[i]+topn[i+1])/2), textcoords='data',
-            fontsize=24.)
-        
-        ax.annotate(("Layer %1i")%(i),
-            xy=(widthg/2., (topn[i]+topn[i+1])/2), xycoords='data',
-            xytext=(widthg/2., (topn[i]+topn[i+1])/2), textcoords='data',
-            fontsize=20.)
-        
-        ax.plot(widthn,topn[i]*np.ones_like(widthn),color='black')    
-        ax.fill_between(widthn,topn[i],topn[i+1],alpha=0.3,color="none",edgecolor='black', hatch=hatches[(i-1)%10])
-        
-        
-    ax.annotate(("....."),
-            xy=(0,(topn[-2]+topn[-2+1])/2), xycoords='data',
-            xytext=(0,(topn[-2]+topn[-2+1])/2), textcoords='data',
-            fontsize=32.)
-    
-    ax.plot(widthn,topn[-2]*np.ones_like(widthn),color='black')    
-    ax.fill_between(widthn,topn[-2],topn[-1],alpha=0.3,color="none",edgecolor='black', hatch=hatches[(len(topn)-1)%10])
-        
-    ax.plot(widthn,topn[-1]*np.ones_like(widthn),color='black')    
-    ax.fill_between(widthn,topn[-1],z.max(),alpha=0.3,color="none",edgecolor='black', hatch=hatches[(len(topn)-2)%10])
-    
-    ax.annotate("",
-            xy=(-widthg, topn[-1]), xycoords='data',
-            xytext=(-widthg,z.max()), textcoords='data',
-            arrowprops=dict(arrowstyle='->',color='black',linewidth=2.))
-        
-    ax.annotate(("$\infty$"),
-            xy=(-widthg,(topn[-1]+z.max())/2), xycoords='data',
-            xytext=(-widthg,(topn[-1]+z.max())/2), textcoords='data',
-            fontsize=24.)
-    
-    ax.annotate(("$z_{n-1}$"),
-            xy=(widthg,topn[-1]), xycoords='data',
-            xytext=(widthg,topn[-1]), textcoords='data',
-            fontsize=24.)
-    
-    ax.annotate(("Layer n"),
-            xy=(widthg/2., (topn[-1]+z.max())/2), xycoords='data',
-            xytext=(widthg/2., (topn[-1]+z.max())/2), textcoords='data',
-            fontsize=24.)
-    
-    ax.annotate(("$\sigma_n$"),
-            xy=(0., (topn[-1]+z.max())/2), xycoords='data',
-            xytext=(0., (topn[-1]+z.max())/2), textcoords='data',
-            fontsize=24.)
-    
-    ax.annotate(("$\epsilon_n$"),
-            xy=(-widthg/1.5, (topn[-1]+z.max())/2), xycoords='data',
-            xytext=(-widthg/1.5, (topn[-1]+z.max())/2), textcoords='data',
-            fontsize=24.)
-    
-    ax.annotate(("$\mu_n$"),
-            xy=(-widthg/2., (topn[-1]+z.max())/2), xycoords='data',
-            xytext=(-widthg/2., (topn[-1]+z.max())/2), textcoords='data',
-            fontsize=20.)
-    
-    ax.annotate("",
-            xy=(widthg/2., -1.*z.max()/5.), xycoords='data',
-            xytext=(widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.2,head_length=1.2',color='green',linewidth=2.)
-            )
-
-    ax.annotate("",
-            xy=(widthg/2., -3./4.*z.max()/5.), xycoords='data',
-            xytext=(widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.4,head_length=1.4',color='green',linewidth=2.)
-            )
-
-    ax.annotate("",
-            xy=(widthg/2., -1./2.*z.max()/5.), xycoords='data',
-            xytext=(widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.6,head_length=1.6',color='green',linewidth=2.)
-            )
-    
-    ax.annotate("",
-            xy=(1.2*widthg/2., -1.*z.max()/5.), xycoords='data',
-            xytext=(1.2*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.2,head_length=1.2',color='green',linewidth=2.)
-            )
-
-    ax.annotate("",
-            xy=(1.2*widthg/2., -3./4.*z.max()/5.), xycoords='data',
-            xytext=(1.2*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.4,head_length=1.4',color='green',linewidth=2.)
-            )
-
-    ax.annotate("",
-            xy=(1.2*widthg/2., -1./2.*z.max()/5.), xycoords='data',
-            xytext=(1.2*widthg/2., 0.), textcoords='data',
-            arrowprops=dict(arrowstyle='->, head_width=1.6,head_length=1.6',color='green',linewidth=2.)
-            )
-    
     ax.annotate("",
             xy=(1.5*widthg/2., -1.*z.max()/5.), xycoords='data',
             xytext=(1.5*widthg/2., 0.), textcoords='data',
@@ -389,6 +238,7 @@ def PlotLayer(thick,ax,widthg,z):
     return ax
 
 #Propagate Up and Down component for a certain frequency & evaluate E and H field
+
 def Propagate(f,H,sig,chg,taux,c,mu,eps,n):
    
     sigcm = np.zeros_like(sig,dtype='complex_')
@@ -396,7 +246,7 @@ def Propagate(f,H,sig,chg,taux,c,mu,eps,n):
     for j in range(1,len(sig)):
         sigcm[j]=PCC(sig[j],chg[j],taux[j],c[j],f)
     
-    K = kwave(mu,sigcm,eps,f)
+    K = k(mu,sigcm,eps,f)
     Z = ImpZ(f,mu,K)
     
     EH = np.matrix(np.zeros((2,n+1),dtype = 'complex_'),dtype = 'complex_')
@@ -406,7 +256,7 @@ def Propagate(f,H,sig,chg,taux,c,mu,eps,n):
     
     for i in range(-2,-(n+2),-1):
         
-        UD[:,i] = transi(H[i+1],K[i])*propinv(Z[i])*prop(Z[i+1])*UD[:,i+1]
+        UD[:,i] = Tinv(H[i+1],K[i])*Pinv(Z[i])*P(Z[i+1])*UD[:,i+1]
         UD = UD/((np.abs(UD[0,:]+UD[1,:])).max())
     
     for j in range(0,n+1):  
@@ -428,13 +278,13 @@ def appres(F,H,sig,chg,taux,c,mu,eps,n):
         
         App_ImpZ[i] = EH[0,1]/EH[1,1]
         
-        Res[i] = np.abs(App_ImpZ[i])**2./(mu_0*2.*np.pi*F[i])
+        Res[i] = np.abs(App_ImpZ[i])**2./(mu_0*omega(F[i]))
         Phase[i] = np.angle(App_ImpZ[i], deg = True)
         
     return Res,Phase
 
 #Evaluate Up, Down components, E and H field, for a frequency range, 
-#a discretized depth range and a time range (Movie)  
+#a discretized depth range and a time range (use to calculate envelope)  
 def calculateEHzt(F,H,sig,chg,taux,c,mu,eps,n,zsample,tsample):
     
     topc = top(H)
@@ -462,8 +312,8 @@ def calculateEHzt(F,H,sig,chg,taux,c,mu,eps,n,zsample,tsample):
                 
                 Exzt[p,q]  = Exzt[p,q] + E_ZT(UDaux[0,p],UDaux[1,p],F[j],tsample[q])/len(F)
                 Hyzt[p,q] = Hyzt[p,q] + H_ZT(UDaux[0,p],UDaux[1,p],Z[layer[p]],F[j],tsample[q])/len(F)
-                Uz[p,q] = Uz[p,q] + UDaux[0,p]*np.exp(1j*2*np.pi*F[j]*tsample[q])/len(F)
-                Dz[p,q] = Dz[p,q] + UDaux[1,p]*np.exp(1j*2*np.pi*F[j]*tsample[q])/len(F)
+                Uz[p,q] = Uz[p,q] + UDaux[0,p]*np.exp(1j*omega(F[j])*tsample[q])/len(F)
+                Dz[p,q] = Dz[p,q] + UDaux[1,p]*np.exp(1j*omega(F[j])*tsample[q])/len(F)
     
     return  Exzt,Hyzt,Uz,Dz,UDaux,layer
     
@@ -489,10 +339,7 @@ def PlotAppRes(F,H,sig,chg,taux,c,mu,eps,n,fenvelope,PlotEnvelope):
     ax0.set_xlim([0.,90.])
     ax0.set_ylim([F.min(),F.max()])
     ax0.scatter(Phase,F,color='purple')
-    #ax[1].set_yscale('Log')
-    #ax[1].grid(which='major')
     ax0.set_xlabel('Phase (Degrees)',fontsize=16.,color="purple")
-    #ax[1].set_ylabel('Frequency (Hz)')
     
     zc=np.arange(-(H[1:].max()+10)*n,(H[1:].max()+10)*n,10.)
     
@@ -517,9 +364,7 @@ def PlotAppRes(F,H,sig,chg,taux,c,mu,eps,n,fenvelope,PlotEnvelope):
         ax[1].set_xlabel('Amplitude Electric Field E (V/m)',color='blue',fontsize=16)
 
         ax1.set_xlabel('Amplitude Magnetic Field H (A/m)',color='red',fontsize=16)
-        
-        #ax1.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-        
+                
         ax[1].fill_betweenx(zc,np.squeeze(np.asarray(np.real(Exzt.min(axis=1)))),
                       np.squeeze(np.asarray(np.real(Exzt.max(axis=1)))), 
                       color='blue', alpha=0.1)
@@ -530,14 +375,12 @@ def PlotAppRes(F,H,sig,chg,taux,c,mu,eps,n,fenvelope,PlotEnvelope):
        
         ax[1] = PlotConfiguration(H,sig,eps,mu,ax[1],(1.5*np.abs(Exzt).max()),zc)
         ax1.set_xlim([-1.5*np.abs(Hyzt).max(),1.5*np.abs(Hyzt).max()])
-        #ax1.plot(np.zeros(2), np.r_[-30, 20])
         ax1.set_xlim([-1.5*np.abs(Hyzt).max(),1.5*np.abs(Hyzt).max()])
     else:
         print 'No envelop (if True, might be slow)'
         ax[1] = PlotConfiguration(H,sig,eps,mu,ax[1],1.,zc)
         ax[1].get_xaxis().set_ticks([])
     
-    plt.tight_layout()
     plt.show()
 
 #Interactive MT for Notebook
@@ -571,13 +414,28 @@ def PlotAppRes3LayersInteract(h1,h2,sigl1,sigl2,sigl3,mul1,mul2,mul3,epsl1,epsl2
     PlotAppRes(frangn,thick3,sig3,chg3_0,taux3,c3,mu3,eps3,3,F_Envelope,PlotEnvelope)
     
 
-def run(plotIt=True):
+def run(n,plotIt=True):
     # something to make a plot
-    return None
-    PlotAppRes(F, H, sig, chg, taux, c, mu, eps, n, fenvelope, PlotEnvelope) 
+
+    F = frange(-5.,5.,20)
+    H = thick(50.,100.,n)
+    sign = sig(-5.,0.,n)
+    mun = mu(1.,2.,n)
+    epsn = eps(1.,9.,n)
+    chg = np.zeros_like(sign)
+    taux = np.zeros_like(sign)
+    c = np.zeros_like(sign)
+
+    Res, Phase = appres(F,H,sign,chg,taux,c,mun,epsn,n)
+
+    if plotIt:
+
+        PlotAppRes(F, H, sign, chg, taux, c, mun, epsn, n, fenvelope=1000., PlotEnvelope=True) 
+
+    return Res, Phase
 
 if __name__ == '__main__':
-    run()
+    run(3)
        
     
     
