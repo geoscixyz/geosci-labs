4571e88025d27752d3b3546fef2a67b61f479ec9
Lindsey Heagy <lindseyheagy@gmail.com>
2016-07-24 11:53:53 -0700

    better error handling

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -3,32 +3,43 @@ import sys
 import os
 import subprocess
 
-# Testing for the notebooks - use nbconvert to execute all cells of the notebook
+# Testing for the notebooks - use nbconvert to execute all cells of the
+# notebook
 
 TestDir = os.path.abspath('../') # where are the notebooks?
 
+
 def setUp():
-    nbpaths = [] #list of notebooks, with file paths
-    nbnames = [] # list of notebook names (for making the tests)
+    nbpaths = []  # list of notebooks, with file paths
+    nbnames = []  # list of notebook names (for making the tests)
 
     # walk the test directory and find all notebooks
     for dirname, dirnames, filenames in os.walk(TestDir):
         for filename in filenames:
             if filename.endswith('.ipynb') and not filename.endswith('-checkpoint.ipynb'):
-                nbpaths.append(os.path.abspath(dirname) + os.path.sep + filename) #get abspath of notebook
+                nbpaths.append(os.path.abspath(dirname) + os.path.sep + filename) # get abspath of notebook
                 nbnames.append(''.join(filename[:-6])) # strip off the file extension
     return nbpaths, nbnames
+
 
 def get(nbname, nbpath):
 
     # use nbconvert to execute the notebook
     def test_func(self):
-        print '\nTesting {0}'.format(nbname)
-        print os.path.abspath('./')
-        print '{0}'.format(nbpath)
-        check = subprocess.call(['jupyter', 'nbconvert', '{0}'.format(nbpath), '--execute'])
-        assert check == 0
-        subprocess.call(['rm', '{0}.html'.format(nbname)])
+        print '\n-------- Testing {0} --------'.format(nbname)
+        print '   {0}'.format(nbpath)
+        check = subprocess.call(['jupyter', 'nbconvert', '{0}'.format(nbpath),
+                                 '--execute'])
+        if check == 0:
+            print '\n ..... {0} Passed ..... \n'.format(nbname)
+        else:
+            print '\n <<<<< {0} FAILED >>>>> \n'.format(nbname)
+
+        self.assertTrue(check == 0, ('{0} FAILED :( '.format(nbname)))
+
+        subprocess.call(['rm', '{0}'.format(os.path.abspath('./') +
+                                            os.path.sep +
+                                            nbname + '.html')])
 
     return test_func
 
