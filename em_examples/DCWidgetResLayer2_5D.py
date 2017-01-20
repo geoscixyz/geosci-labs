95be1b7c7d0fe31d619fd6f948e4661264ba891f
micmitch <mmitchel@eos.ubc.ca>
2017-01-20 11:26:19 -0800

    Fixed bugs pointed out by Seogi.

--- em_examples/DCWidgetResLayer2_5D.py
+++ em_examples/DCWidgetResLayer2_5D.py
@@ -72,7 +72,10 @@ def model_fields(A,B,zcLayer,dzLayer,xc,zc,r,sigLayer,sigTarget,sigHalf):
     # Nx = np.empty(shape=(mesh.nC, 2))
     rx = DC.Rx.Pole_ky(Mx)
     # rx = DC.Rx.Dipole(Mx,Nx)
-    src = DC.Src.Dipole([rx], np.r_[A,0.], np.r_[B,0.])
+    if(B == []):
+        src = DC.Src.Pole([rx], np.r_[A,0.])
+    else:
+        src = DC.Src.Dipole([rx], np.r_[A,0.], np.r_[B,0.])
     # src = DC.Src.Dipole_ky([rx], np.r_[A,0.], np.r_[B,0.])
     survey = DC.Survey_ky([src])
     # survey = DC.Survey([src])
@@ -723,14 +726,14 @@ def plot_Surface_Potentials(survey,A,B,M,N,zcLayer,dzLayer,xc,zc,r,rhohalf,rhola
 def ResLayer_app():
     app = interact(plot_Surface_Potentials,
                 survey = ToggleButtons(options =['Dipole-Dipole','Dipole-Pole','Pole-Dipole','Pole-Pole'],value='Dipole-Dipole'),
-                zcLayer = FloatSlider(min=-10.,max=0.,step=1.,value=-10., continuous_update=False),
-                dzLayer = FloatSlider(min=0.5,max=5.,step=0.5,value=1., continuous_update=False),
-                rholayer = FloatText(min=1e-8,max=1e8, value = 5000., continuous_update=False,description='$\\rho_{Layer}$'),
+                zcLayer = FloatSlider(min=-10.,max=0.,step=1.,value=-10., continuous_update=False,description='$zc_{layer}$'),
+                dzLayer = FloatSlider(min=0.5,max=5.,step=0.5,value=1., continuous_update=False,description='$dz_{layer}$'),
+                rholayer = FloatText(min=1e-8,max=1e8, value = 5000., continuous_update=False,description='$\\rho_{2}$'),
                 xc = FloatSlider(min=-30.,max=30.,step=1.,value=0., continuous_update=False),
                 zc = FloatSlider(min=-30.,max=-15.,step=0.5,value=-25., continuous_update=False),
                 r = FloatSlider(min=1.,max=10.,step=0.5,value=5., continuous_update=False),
-                rhoTarget = FloatText(min=1e-8,max=1e8, value = 500., continuous_update=False,description='$\\rho_{Cyl}$'),
-                rhohalf = FloatText(min=1e-8,max=1e8, value = 500., continuous_update=False,description='$\\rho_{Half}$'),
+                rhoTarget = FloatText(min=1e-8,max=1e8, value = 500., continuous_update=False,description='$\\rho_{3}$'),
+                rhohalf = FloatText(min=1e-8,max=1e8, value = 500., continuous_update=False,description='$\\rho_{1}$'),
                 A = FloatSlider(min=-30.25,max=30.25,step=0.5,value=-30.25, continuous_update=False),
                 B = FloatSlider(min=-30.25,max=30.25,step=0.5,value=30.25, continuous_update=False),
                 M = FloatSlider(min=-30.25,max=30.25,step=0.5,value=-10.25, continuous_update=False),
