4f53b98c444cb7cea00e9aa46ec25d688de8a753
Lindsey Heagy <lindseyheagy@gmail.com>
2017-01-22 11:17:04 -0800

    add meta to __init__.py

--- em_examples/__init__.py
+++ em_examples/__init__.py
@@ -29,6 +29,10 @@ import View
 import VolumeWidget
 import VolumeWidgetPlane
 
+__version__   = '0.0.1'
+__author__    = 'GeoScixyz developers'
+__license__   = 'MIT'
+__copyright__ = 'Copyright 2017 GeoScixyz developers'
 
 
 
