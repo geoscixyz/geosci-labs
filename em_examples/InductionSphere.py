32bdfce8c2f5d105bf84c9ba7c454e1264f3e96e
dccowan <devinccowan@gmail.com>
2017-05-25 13:38:48 -0700

    Changes to functions

--- em_examples/InductionSphere.py
+++ em_examples/InductionSphere.py
@@ -31,7 +31,7 @@ class IndSphere():
     """
     
 
-    def __init__(self,m,m_dir,h,sig,mur,a,x0,y0,z0):
+    def __init__(self,m,m_dir,sig,mur,a,x0,y0,z0,xtx,ytx,ztx,xrx,yrx,zrx):
         """Defines Initial Attributes"""
         
         # INITIALIZES OBJECT
@@ -48,13 +48,18 @@ class IndSphere():
         
         self.m = m
         self.m_dir = m_dir
-        self.h = h
         self.sig = sig
         self.mur = mur
         self.a = a
         self.x0 = x0
         self.y0 = y0
         self.z0 = z0
+        self.xtx = xtx
+        self.ytx = ytx
+        self.ztx = ztx
+        self.xrx = xrx
+        self.yrx = yrx
+        self.zrx = zrx
 
 ############################################
 #   DEFINE METHODS
@@ -67,25 +72,27 @@ class IndSphere():
 
     	m = self.m
         m_dir = self.m_dir
-        h = self.h
+        xtx = self.xtx
+        ytx = self.ytx
+        ztx = self.ztx
         x0 = self.x0
         y0 = self.y0
         z0 = self.z0
 
-        R = np.sqrt(x0**2 + y0**2 + (z0-h)**2)
+        R = np.sqrt((x0-xtx)**2 + (y0-ytx)**2 + (z0-ztx)**2)
 
     	if m_dir == "x":
-    		self.Hpx = (1/(4*np.pi))*(3*m*x0*x0/R**5 - m/R**3)
-    		self.Hpy = (1/(4*np.pi))*(3*m*y0*x0/R**5)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-h)*x0/R**5)
+    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(x0-xtx)/R**5 - m/R**3)
+    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(x0-xtx)/R**5)
+    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(x0-xtx)/R**5)
     	elif m_dir == "y":
-    		self.Hpx = (1/(4*np.pi))*(3*m*x0*y0/R**5)
-    		self.Hy = (1/(4*np.pi))*(3*m*y0*y0/R**5 - m/R**3)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-h)*y0/R**5)
+    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(y0-ytx)/R**5)
+    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(y0-ytx)/R**5 - m/R**3)
+    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(y0-ytx)/R**5)
     	elif m_dir == "z":
-    		self.Hpx = (1/(4*np.pi))*(3*m*x0*(z0-h)/R**5)
-    		self.Hpy = (1/(4*np.pi))*(3*m*y0*(z0-h)/R**5)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-h)*(z0-h)/R**5 - m/R**3)
+    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(z0-ztx)/R**5)
+    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(z0-ztx)/R**5)
+    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(z0-ztx)/R**5 - m/R**3)
 
     global fcn_ComputeExcitation_FEM
     def fcn_ComputeExcitation_FEM(self,f):
@@ -103,24 +110,27 @@ class IndSphere():
 
         return chi
 
-    def fcn_ComputeFrequencyResponse(self,f,X,Y,Z):
+    def fcn_ComputeFrequencyResponse(self,f):
     	"""Compute Spectrum at a single location (X,Y,Z)"""
 
         a = self.a
         x0 = self.x0
         y0 = self.y0
         z0 = self.z0
+        xrx = self.xrx
+        yrx = self.yrx
+        zrx = self.zrx
 
         chi = fcn_ComputeExcitation_FEM(self,f)
 
         mx = 4*np.pi*a**3*chi*self.Hpx/3
         my = 4*np.pi*a**3*chi*self.Hpy/3
         mz = 4*np.pi*a**3*chi*self.Hpz/3
-        R = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
+        R = np.sqrt((xrx-x0)**2 + (yrx-y0)**2 + (zrx-z0)**2)
 
-        Hx = (1/(4*np.pi))*(3*(X-x0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mx/R**3)
-        Hy = (1/(4*np.pi))*(3*(Y-y0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - my/R**3)
-        Hz = (1/(4*np.pi))*(3*(Z-z0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mz/R**3)
+        Hx = (1/(4*np.pi))*(3*(xrx-x0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - mx/R**3)
+        Hy = (1/(4*np.pi))*(3*(yrx-y0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - my/R**3)
+        Hz = (1/(4*np.pi))*(3*(zrx-z0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - mz/R**3)
         Habs = np.sqrt(Hx**2 + Hy**2 + Hz**2)
 
         return chi, Hx, Hy, Hz, Habs
@@ -193,7 +203,15 @@ def plotResponseFEM(Ax,f,H,Comp):
 
 #def plotResponsePlaneFEM(Ax,f,X,Y,Z,H,Comp):
 
-	#FS = 20
+#	FS = 20
+
+#	tol = 1e-4
+#	Habs = np.abs(H)
+#	MAX = np.max(np.abs(Habs))
+#	Hsign = np.sign(H)
+
+
+
 
 
 
