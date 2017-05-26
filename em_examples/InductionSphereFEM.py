036092031ab21635c51113dfe57929b3324f3af5
dccowan <devinccowan@gmail.com>
2017-05-26 16:59:41 -0700

    Updates to sphere apps

--- em_examples/InductionSphereFEM.py
+++ em_examples/InductionSphereFEM.py
@@ -9,7 +9,7 @@ from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
 #   PLOTTING FUNCTIONS FOR WIDGETS
 ##############################################
 
-def fcn_FDEM_InductionSpherePlaneWidget(xtx,ytx,ztx,m,m_dir,x0,y0,z0,a,sig,mur,xrx,yrx,zrx,f,Comp,Phase):
+def fcn_FDEM_InductionSpherePlaneWidget(xtx,ytx,ztx,m,orient,x0,y0,z0,a,sig,mur,xrx,yrx,zrx,f,Comp,Phase):
 
 	fvec = np.logspace(0,8,49)
 
@@ -18,10 +18,9 @@ def fcn_FDEM_InductionSpherePlaneWidget(xtx,ytx,ztx,m,m_dir,x0,y0,z0,a,sig,mur,x
 	X = np.transpose(X)
 	Y = np.transpose(Y)
 
-	Obj = SpherePlaneFEM(m,m_dir)
-	Obj.fcn_ComputePrimary(xtx,ytx,ztx,x0,y0,z0)
+	Obj = SphereFEM(m,orient,xtx,ytx,ztx)
 
-	Hx,Hy,Hz,Habs = Obj.fcn_ComputeFrequencyResponsePlane(f,sig,mur,a,x0,y0,z0,X,Y,zrx)
+	Hx,Hy,Hz,Habs = Obj.fcn_ComputeFrequencyResponse(f,sig,mur,a,x0,y0,z0,X,Y,zrx)
 	Hxi,Hyi,Hzi,Habsi = Obj.fcn_ComputeFrequencyResponse(fvec,sig,mur,a,x0,y0,z0,xrx,yrx,zrx)
 
 	fig1 = plt.figure(figsize=(17,6))
@@ -29,28 +28,59 @@ def fcn_FDEM_InductionSpherePlaneWidget(xtx,ytx,ztx,m,m_dir,x0,y0,z0,a,sig,mur,x
 	Ax2 = fig1.add_axes([0.6,0,0.4,1])
 
 	if Comp == 'x':
-		Ax1 = plotResponsePlaneFEM(Ax1,f,X,Y,ztx,Hx,Comp,Phase)
-		Ax1 = plotPlaceTxRxSphere(Ax1,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a)
+		Ax1 = plotAnomalyXYplane(Ax1,f,X,Y,ztx,Hx,Comp,Phase)
+		Ax1 = plotPlaceTxRxSphereXY(Ax1,xtx,ytx,xrx,yrx,x0,y0,a)
 		Ax2 = plotResponseFEM(Ax2,fvec,Hxi,Comp)
 	elif Comp == 'y':
-		Ax1 = plotResponsePlaneFEM(Ax1,f,X,Y,ztx,Hy,Comp,Phase)
-		Ax1 = plotPlaceTxRxSphere(Ax1,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a)
+		Ax1 = plotAnomalyXYplane(Ax1,f,X,Y,ztx,Hy,Comp,Phase)
+		Ax1 = plotPlaceTxRxSphereXY(Ax1,xtx,ytx,xrx,yrx,x0,y0,a)
 		Ax2 = plotResponseFEM(Ax2,fvec,Hyi,Comp)
 	elif Comp == 'z':
-		Ax1 = plotResponsePlaneFEM(Ax1,f,X,Y,ztx,Hz,Comp,Phase)
-		Ax1 = plotPlaceTxRxSphere(Ax1,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a)
+		Ax1 = plotAnomalyXYplane(Ax1,f,X,Y,ztx,Hz,Comp,Phase)
+		Ax1 = plotPlaceTxRxSphereXY(Ax1,xtx,ytx,xrx,yrx,x0,y0,a)
 		Ax2 = plotResponseFEM(Ax2,fvec,Hzi,Comp)
 	elif Comp == 'abs':
-		Ax1 = plotResponsePlaneFEM(Ax1,f,X,Y,ztx,Habs,Comp,Phase)
-		Ax1 = plotPlaceTxRxSphere(Ax1,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a)
+		Ax1 = plotAnomalyXYplane(Ax1,f,X,Y,ztx,Habs,Comp,Phase)
+		Ax1 = plotPlaceTxRxSphereXY(Ax1,xtx,ytx,xrx,yrx,x0,y0,a)
 		Ax2 = plotResponseFEM(Ax2,fvec,Habsi,Comp)
-
 
 	plt.show(fig1)
 
 
+def fcn_FDEM_InductionSphereProfileWidget(xtx,ztx,m,orient,x0,z0,a,sig,mur,xrx,zrx,f,Phase):
+
+	ytx = 0.
+	y0 = 0.
+	yrx = 0.
+	Y = 0.
+
+	fvec = np.logspace(0,8,49)
+
+	xmin, xmax, dx, zmin, zmax, dz = -20., 20., 0.25, -20., 20., 0.25
+	X,Z = np.mgrid[xmin:xmax+dx:dx, zmin:zmax+dz:dz]
+	X = np.transpose(X)
+	Z = np.transpose(Z)
+
+	Obj = SphereFEM(m,orient,xtx,ytx,ztx)
+
+	Hx,Hy,Hz,Habs = Obj.fcn_ComputeFrequencyResponse(f,sig,mur,a,x0,y0,z0,X,Y,Z)
+	Hxi,Hyi,Hzi,Habsi = Obj.fcn_ComputeFrequencyResponse(fvec,sig,mur,a,x0,y0,z0,xrx,yrx,zrx)
+
+	fig1 = plt.figure(figsize=(17,6))
+	Ax1 = fig1.add_axes([0.04,0,0.42,1])
+	Ax2 = fig1.add_axes([0.6,0,0.4,1])
+
+	if orient == 'x':
+		Ax2 = plotResponseFEM(Ax2,fvec,Hxi,orient)
+	elif orient == 'z':
+		Ax2 = plotResponseFEM(Ax2,fvec,Hzi,orient)
+
+	plt.show(fig1)
+
+
+
 ##############################################
-#   EXCITATION FUNCTIONS
+#   GLOBAL FUNTIONS
 ##############################################
 
 def fcn_ComputeExcitation_FEM(f,sig,mur,a):
@@ -64,152 +94,31 @@ def fcn_ComputeExcitation_FEM(f,sig,mur,a):
 
     return chi
 
-
-############################################
-#   DEFINE CLASSES
-############################################
-
-class SpherePlaneFEM():
-    """Fucntionwhcihdf
-    Input variables:
-        
-        Output variables:
-    """
-    
-
-    def __init__(self,m,m_dir):
-        """Defines Initial Attributes"""
-        
-        # INITIALIZES OBJECT
-        
-        # m: Transmitter dipole moment
-        # m_dir: Transmitter dipole orentation 'x', 'y' or 'z'
-        
-        self.m = m
-        self.m_dir = m_dir
-
-    def fcn_ComputePrimary(self,xtx,ytx,ztx,x0,y0,z0):
+def fcn_ComputePrimary(m,orient,xtx,ytx,ztx,X,Y,Z):
     	"""Computes Inducing Field at Sphere"""
 
-    	m = self.m
-    	m_dir = self.m_dir
+        R = np.sqrt((X-xtx)**2 + (Y-ytx)**2 + (Z-ztx)**2)
 
-        R = np.sqrt((x0-xtx)**2 + (y0-ytx)**2 + (z0-ztx)**2)
+    	if orient == "x":
+    		Hpx = (1/(4*np.pi))*(3*m*(X-xtx)*(X-xtx)/R**5 - m/R**3)
+    		Hpy = (1/(4*np.pi))*(3*m*(Y-ytx)*(X-xtx)/R**5)
+    		Hpz = (1/(4*np.pi))*(3*m*(Z-ztx)*(X-xtx)/R**5)
+    	elif orient == "y":
+    		Hpx = (1/(4*np.pi))*(3*m*(X-xtx)*(Y-ytx)/R**5)
+    		Hpy = (1/(4*np.pi))*(3*m*(Y-ytx)*(Y-ytx)/R**5 - m/R**3)
+    		Hpz = (1/(4*np.pi))*(3*m*(Z-ztx)*(Y-ytx)/R**5)
+    	elif orient == "z":
+    		Hpx = (1/(4*np.pi))*(3*m*(X-xtx)*(Z-ztx)/R**5)
+    		Hpy = (1/(4*np.pi))*(3*m*(Y-ytx)*(Z-ztx)/R**5)
+    		Hpz = (1/(4*np.pi))*(3*m*(Z-ztx)*(Z-ztx)/R**5 - m/R**3)
 
-    	if m_dir == "x":
-    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(x0-xtx)/R**5 - m/R**3)
-    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(x0-xtx)/R**5)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(x0-xtx)/R**5)
-    	elif m_dir == "y":
-    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(y0-ytx)/R**5)
-    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(y0-ytx)/R**5 - m/R**3)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(y0-ytx)/R**5)
-    	elif m_dir == "z":
-    		self.Hpx = (1/(4*np.pi))*(3*m*(x0-xtx)*(z0-ztx)/R**5)
-    		self.Hpy = (1/(4*np.pi))*(3*m*(y0-ytx)*(z0-ztx)/R**5)
-    		self.Hpz = (1/(4*np.pi))*(3*m*(z0-ztx)*(z0-ztx)/R**5 - m/R**3)
+    	return Hpx, Hpy, Hpz
 
-    def fcn_ComputeFrequencyResponse(self,f,sig,mur,a,x0,y0,z0,xrx,yrx,zrx):
-    	"""Compute Spectrum at a single location (X,Y,Z)"""
+##############################################
+#   GLOBAL PLOTTING FUNTIONS
+##############################################
 
-        chi = fcn_ComputeExcitation_FEM(f,sig,mur,a)
-
-        mx = 4*np.pi*a**3*chi*self.Hpx/3
-        my = 4*np.pi*a**3*chi*self.Hpy/3
-        mz = 4*np.pi*a**3*chi*self.Hpz/3
-        R = np.sqrt((xrx-x0)**2 + (yrx-y0)**2 + (zrx-z0)**2)
-
-        Hx = (1/(4*np.pi))*(3*(xrx-x0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - mx/R**3)
-        Hy = (1/(4*np.pi))*(3*(yrx-y0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - my/R**3)
-        Hz = (1/(4*np.pi))*(3*(zrx-z0)*(mx*(xrx-x0) + my*(yrx-y0) + mz*(zrx-z0))/R**5 - mz/R**3)
-        Habs = np.sqrt(np.real(Hx)**2 + np.real(Hy)**2 + np.real(Hz)**2) + 1j*np.sqrt(np.imag(Hx)**2 + np.imag(Hy)**2 + np.imag(Hz)**2)
-
-        return Hx, Hy, Hz, Habs
-
-    def fcn_ComputeFrequencyResponsePlane(self,f,sig,mur,a,x0,y0,z0,X,Y,Z):
-    	"""Compute Single Frequency Response at (X,Y,Z)"""
-
-        chi = fcn_ComputeExcitation_FEM(f,sig,mur,a)
-
-        mx = 4*np.pi*a**3*chi*self.Hpx/3
-        my = 4*np.pi*a**3*chi*self.Hpy/3
-        mz = 4*np.pi*a**3*chi*self.Hpz/3
-        R = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
-
-        Hx = (1/(4*np.pi))*(3*(X-x0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mx/R**3)
-        Hy = (1/(4*np.pi))*(3*(Y-y0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - my/R**3)
-        Hz = (1/(4*np.pi))*(3*(Z-z0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mz/R**3)
-        Habs = np.sqrt(np.real(Hx)**2 + np.real(Hy)**2 + np.real(Hz)**2) + 1j*np.sqrt(np.imag(Hx)**2 + np.imag(Hy)**2 + np.imag(Hz)**2)
-
-        return Hx, Hy, Hz, Habs
-
-
-class SphereProfileFEM():
-    """Fucntionwhcihdf
-    Input variables:
-        
-        Output variables:
-    """
-    
-
-    def __init__(self,m,orient):
-        """Defines Initial Attributes"""
-        
-        # INITIALIZES OBJECT
-        
-        # m: Transmitter dipole moment
-        # m_dir: Transmitter dipole orentation 'x', 'y' or 'z'
-        
-        self.m = m
-        self.orient = orient
-
-
-
-
-
-############################################
-#   DEFINE PLOTTING FUNCTIONS
-############################################
-
-def plotResponseFEM(Ax,f,H,Comp):
-
-	FS = 20
-
-	xTicks = (np.logspace(np.log(np.min(f)),np.log(np.max(f)),9))
-	
-	Ax.grid('both', linestyle='-', linewidth=0.8, color=[0.8, 0.8, 0.8])
-	Ax.semilogx(f,0*f,color='k',linewidth=2)
-	Ax.semilogx(f,np.real(H),color='k',linewidth=4,label="Real")
-	Ax.semilogx(f,np.imag(H),color='k',linewidth=4,ls='--',label="Imaginary")
-	Ax.set_xbound(np.min(f),np.max(f))
-	Ax.set_xlabel('Frequency [Hz]',fontsize=FS+2)
-	Ax.tick_params(labelsize=FS-2)
-	Ax.yaxis.set_major_formatter(FormatStrFormatter('%.1e'))
-
-	if Comp == 'x':
-		Ax.set_ylabel('$\mathbf{Hx}$ [A/m]',fontsize=FS+4,labelpad=-5)
-		Ax.set_title('$\mathbf{Hx}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
-	elif Comp == 'y':
-		Ax.set_ylabel('$\mathbf{Hy}$ [A/m]',fontsize=FS+4,labelpad=-5)
-		Ax.set_title('$\mathbf{Hy}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
-	elif Comp == 'z':
-		Ax.set_ylabel('$\mathbf{Hz}$ [A/m]',fontsize=FS+4,labelpad=-5)
-		Ax.set_title('$\mathbf{Hz}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
-	elif Comp == 'abs':
-		Ax.set_ylabel('$\mathbf{|H|}$ [A/m]',fontsize=FS+4,labelpad=-5)
-		Ax.set_title('$\mathbf{|H|}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
-
-	if np.max(np.real(H[-1])) > 0.:
-		handles, labels = Ax.get_legend_handles_labels()
-		Ax.legend(handles, labels, loc='upper left', fontsize=FS)
-	elif np.max(np.real(H[-1])) < 0.:
-		handles, labels = Ax.get_legend_handles_labels()
-		Ax.legend(handles, labels, loc='lower left', fontsize=FS)
-
-	return Ax
-
-
-def plotResponsePlaneFEM(Ax,f,X,Y,Z,H,Comp,Phase):
+def plotAnomalyXYplane(Ax,f,X,Y,Z,H,Comp,Phase):
 
 	FS = 20
 
@@ -268,7 +177,8 @@ def plotResponsePlaneFEM(Ax,f,X,Y,Z,H,Comp,Phase):
 
 	return Ax
 
-def plotPlaceTxRxSphere(Ax,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a):
+def plotPlaceTxRxSphereXY(Ax,xtx,ytx,xrx,yrx,x0,y0,a):
+
 
 	Xlim = Ax.get_xlim()
 	Ylim = Ax.get_ylim()
@@ -289,6 +199,115 @@ def plotPlaceTxRxSphere(Ax,xtx,ytx,ztx,xrx,yrx,zrx,x0,y0,z0,a):
 	Ax.set_ybound(Ylim)
 
 	return Ax
+
+def plotResponseFEM(Ax,f,H,Comp):
+
+	FS = 20
+
+	xTicks = (np.logspace(np.log(np.min(f)),np.log(np.max(f)),9))
+	
+	Ax.grid('both', linestyle='-', linewidth=0.8, color=[0.8, 0.8, 0.8])
+	Ax.semilogx(f,0*f,color='k',linewidth=2)
+	Ax.semilogx(f,np.real(H),color='k',linewidth=4,label="Real")
+	Ax.semilogx(f,np.imag(H),color='k',linewidth=4,ls='--',label="Imaginary")
+	Ax.set_xbound(np.min(f),np.max(f))
+	Ax.set_xlabel('Frequency [Hz]',fontsize=FS+2)
+	Ax.tick_params(labelsize=FS-2)
+	Ax.yaxis.set_major_formatter(FormatStrFormatter('%.1e'))
+
+	if Comp == 'x':
+		Ax.set_ylabel('$\mathbf{Hx}$ [A/m]',fontsize=FS+4,labelpad=-5)
+		Ax.set_title('$\mathbf{Hx}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
+	elif Comp == 'y':
+		Ax.set_ylabel('$\mathbf{Hy}$ [A/m]',fontsize=FS+4,labelpad=-5)
+		Ax.set_title('$\mathbf{Hy}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
+	elif Comp == 'z':
+		Ax.set_ylabel('$\mathbf{Hz}$ [A/m]',fontsize=FS+4,labelpad=-5)
+		Ax.set_title('$\mathbf{Hz}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
+	elif Comp == 'abs':
+		Ax.set_ylabel('$\mathbf{|H|}$ [A/m]',fontsize=FS+4,labelpad=-5)
+		Ax.set_title('$\mathbf{|H|}$ Response at $\mathbf{Rx}$',fontsize=FS+6)
+
+	if np.max(np.real(H[-1])) > 0.:
+		handles, labels = Ax.get_legend_handles_labels()
+		Ax.legend(handles, labels, loc='upper left', fontsize=FS)
+	elif np.max(np.real(H[-1])) < 0.:
+		handles, labels = Ax.get_legend_handles_labels()
+		Ax.legend(handles, labels, loc='lower left', fontsize=FS)
+
+	return Ax
+
+
+
+
+
+
+############################################
+#   CLASS: SPHERE TOP VIEW
+############################################
+
+############################################
+#	DEFINE CLASS
+
+class SphereFEM():
+    """Fucntionwhcihdf
+    Input variables:
+        
+        Output variables:
+    """
+    
+    def __init__(self,m,orient,xtx,ytx,ztx):
+        """Defines Initial Attributes"""
+        
+        # INITIALIZES OBJECT
+        
+        # m: Transmitter dipole moment
+        # orient: Transmitter dipole orentation 'x', 'y' or 'z'
+        # xtx: Transmitter x location
+        # ytx: Transmitter y location
+        # ztx: Transmitter z location
+
+        self.m = m
+        self.orient = orient
+        self.xtx = xtx
+        self.ytx = ytx
+        self.ztx = ztx
+
+############################################
+#	DEFINE METHODS
+
+    def fcn_ComputeFrequencyResponse(self,f,sig,mur,a,x0,y0,z0,X,Y,Z):
+    	"""Compute Single Frequency Response at (X,Y,Z)"""
+
+        m = self.m
+        orient = self.orient
+        xtx = self.xtx
+        ytx = self.ytx
+        ztx = self.ztx
+
+        chi = fcn_ComputeExcitation_FEM(f,sig,mur,a)
+        Hpx,Hpy,Hpz = fcn_ComputePrimary(m,orient,xtx,ytx,ztx,x0,y0,z0)
+
+        mx = 4*np.pi*a**3*chi*Hpx/3
+        my = 4*np.pi*a**3*chi*Hpy/3
+        mz = 4*np.pi*a**3*chi*Hpz/3
+        R = np.sqrt((X-x0)**2 + (Y-y0)**2 + (Z-z0)**2)
+
+        Hx = (1/(4*np.pi))*(3*(X-x0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mx/R**3)
+        Hy = (1/(4*np.pi))*(3*(Y-y0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - my/R**3)
+        Hz = (1/(4*np.pi))*(3*(Z-z0)*(mx*(X-x0) + my*(Y-y0) + mz*(Z-z0))/R**5 - mz/R**3)
+        Habs = np.sqrt(np.real(Hx)**2 + np.real(Hy)**2 + np.real(Hz)**2) + 1j*np.sqrt(np.imag(Hx)**2 + np.imag(Hy)**2 + np.imag(Hz)**2)
+
+        return Hx, Hy, Hz, Habs
+
+
+
+
+    
+
+
+
+
 
 
 
