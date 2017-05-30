a0864f82ee0b8c5de64a66d135a2e4a48b742a89
Thibaut Astic <thast@eos.ubc.ca>
2017-05-30 11:43:41 -0700

    plt.show() for MT1D

--- em_examples/Base.py
+++ em_examples/Base.py
@@ -1,5 +1,5 @@
 from ipywidgets import (
-    interactive, VBox, HBox, Box, Widget, interact, interact_manual, IntSlider, FloatSlider, FloatText, ToggleButtons, fixed, Checkbox
+    interactive, VBox, HBox, Box, Widget, interact, interact_manual, IntSlider, FloatSlider, FloatText, ToggleButton, ToggleButtons, fixed, Checkbox
 )
 from IPython.display import display
 import matplotlib.pyplot as plt
@@ -15,7 +15,7 @@ class MyApp(Box):
 
     @property
     def kwargs(self):
-        instanceCheck = lambda x: isinstance(x, ToggleButtons) or isinstance(x, FloatSlider) or isinstance(x, IntSlider) or isinstance(x, FloatText) or isinstance(x, fixed) or isinstance(x, Checkbox)
+        instanceCheck = lambda x: isinstance(x, ToggleButton), isinstance(x, ToggleButtons) or isinstance(x, FloatSlider) or isinstance(x, IntSlider) or isinstance(x, FloatText) or isinstance(x, fixed) or isinstance(x, Checkbox)
         return dict(
             [(key, val.value) for key, val in self._kwargs.iteritems()
             if instanceCheck(val)]
