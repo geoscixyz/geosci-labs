45652d56195ba95ecd19177326c6b19a89b891af
Lindsey Heagy <lindseyheagy@gmail.com>
2017-05-29 23:01:43 -0700

    Bump version: 0.0.7 → 0.0.8

--- em_examples/__init__.py
+++ em_examples/__init__.py
@@ -28,7 +28,7 @@ import View
 import VolumeWidget
 import VolumeWidgetPlane
 
-__version__   = '0.0.7'
+__version__   = '0.0.8'
 __author__    = 'GeoScixyz developers'
 __license__   = 'MIT'
 __copyright__ = 'Copyright 2017 GeoScixyz developers'
