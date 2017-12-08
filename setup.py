f743c28534c5996263389c92a08b630823f69941
Lindsey Heagy <lindseyheagy@gmail.com>
2017-12-08 08:53:39 -0800

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open('README.rst') as f:
 
 setup(
     name = 'gpgLabs',
-    version = '0.0.8',
+    version = '0.0.9',
     packages = find_packages(),
     install_requires = [
         'numpy>=1.7',
