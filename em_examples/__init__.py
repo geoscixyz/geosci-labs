93e814fb40e9d197c4bf734b1f79cc6dbae8af63
Thibaut Astic <thast@eos.ubc.ca>
2018-03-03 14:06:08 -0800

    Bump version: 0.0.18 → 0.0.19

--- em_examples/__init__.py
+++ em_examples/__init__.py
@@ -37,7 +37,7 @@ from . import VolumeWidget
 from . import VolumeWidgetPlane
 
 
-__version__ = '0.0.18'
+__version__ = '0.0.19'
 __author__ = 'GeoScixyz developers'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 GeoScixyz developers'
