e273114ca0f1cac47114846aa6ae104556569277
Lindsey Heagy <lindseyheagy@gmail.com>
2018-07-19 09:16:41 -0700

    use python 3.6 in the conda environment

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -3,4 +3,4 @@
 conda env create -f /home/nbuser/library/environment.yml
 source activate em-apps-environment
 
-# pip install -r requirements.txt
+pip install -r /home/nbuser/library/requirements.txt
