7ccb393faeae43678b8c903e84f82224f168fb16
SEOGI KANG <skang@eos.ubc.ca>
2018-07-18 17:42:15 -0700

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Activate environment
+source /home/nbcommon/anaconda3_501/bin/activate
+
+# Set up proxy
+http_proxy=http://webproxy:3128
+https_proxy=http://webproxy:3128
+export http_proxy 
+export https_proxy
+
+# pip
 pip install -r /home/nbuser/library/requirements.txt
