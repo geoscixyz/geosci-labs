5e5037c6831a18d89e7efe57f5d4b1323b25cf7e
Thibaut Astic <thast@eos.ubc.ca>
2019-02-19 21:39:03 -0800

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="geoscilabs",
-    version="0.0.2",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.7",
