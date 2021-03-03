8d7dc9cd75ba700542a65bc0be99331066aeb301
Lindsey Heagy <lindseyheagy@gmail.com>
2021-03-03 11:30:03 -0700

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="geoscilabs",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.7",
